c8224d4464e270da82e140f1e30b75b1f4e3b542
zakijamaliarafi <zakijamaliarafi@gmail.com>
2024-10-02 22:32:13 +0700

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       title: 'Toko Kita',
       debugShowCheckedModeBanner: false,
-      home: ProdukPage(),
+      home: LoginPage(),
     );
   }
 }
